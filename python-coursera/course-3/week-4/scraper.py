9a5b2c6989afa90f7fcd8e5fe1df9cfd08518035
ZheTioS <vinayaksreenath@gmail.com>
2021-04-21 19:46:35 +0200

    Added scraper.py to week-4
    Signed-off-by: ZheTioS <vinayaksreenath@gmail.com>

--- python-coursera/course-3/week-4/scraper.py
+++ python-coursera/course-3/week-4/scraper.py
@@ -1,10 +1,23 @@
-import urllib.request, urllib.parse, urlib.error
+import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+#In case of SSL security issues for HTTPS:// sites few extra steps need to be taken
 
-url = input()
-html = urlib.request.urlopen().read()
-soup = BeautifulSoup(html, 'html.parser')
+#test code with sample data
+#Sample Url : http://py4e-data.dr-chuck.net/comments_42.html
+# url_sample = input("Enter Url :") #Sample Url
+# html_sample = urllib.request.urlopen(url_sample).read() # for sample
+# soup_sample = BeautifulSoup(html_sample, 'html.parser')
+# spans = soup_sample('span')
 
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+#Actual URL : http://py4e-data.dr-chuck.net/comments_1095116.html
+url = input("Enter Url :") 
+html = urllib.request.urlopen(url).read() #Reading the HTML code from the given URL
+soup = BeautifulSoup(html, 'html.parser') #Using beautiful soup to parse the obtained file
+spans = soup('span') # Asigning the tag to be found
+
+numbers = [] # Creating a list to store numbers obtained
+
+for span in spans: #for loop to traverse through the parsed html file
+    numbers.append(int(span.string)) #obtaining the numbers from parsed file
+
+print (sum(numbers)) #printing the sum of numbers obtained
